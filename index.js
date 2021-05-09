971e5fe3bbe523a3f2e2e8efe41b780f46412110
NilsPvR <nilsundgrk@gmx.de>
2021-05-09 19:06:16 +0200

    prune command

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ client.once('ready', () => {
 let loop_stop = false;
 let running = false;
 
+function autodel(msg) {
+	msg.delete({ timeout: 3000 })
+		.catch(console.error);
+}
+
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -70,6 +75,29 @@ client.on('message', message => {
 		});
 
 		message.channel.send(avatarList);
+	}
+	else if (command === 'prune') {
+		const amount = parseInt(args[0]);
+		if (!args.length) {
+			return message.reply('you didn\'t provide a amount to delete');
+		}
+		else if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		}
+		else if (amount === 0) {
+			return message.reply('well I deleted 0 messages...');
+		}
+		else if (amount < 1 || amount > 100) {
+			return message.reply('you need to provide a number which is betweeen 1 and 100.');
+		}
+		else if (amount === 1) {
+			message.channel.bulkDelete(amount + 1, true);
+			return message.reply(`${amount} message has been deleted`)
+				.then(msg => autodel(msg));
+		}
+		message.channel.bulkDelete(amount + 1, true);
+		message.reply(`${amount} messages have been deleted`)
+			.then(msg => autodel(msg));
 	}
 	else if (message.content.startsWith(`${prefix}beep`)) {
 		message.channel.send('Boop.');
