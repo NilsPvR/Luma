0ff37caf85fdee373698f18759718c1a6622cc8e
NilsPvR <nilsundgrk@gmx.de>
2021-05-16 12:32:45 +0200

    fancy args-info

--- commands/info/args-info.js
+++ commands/info/args-info.js
@@ -1,9 +1,11 @@
+const { prefix } = require('../../config.json');
 module.exports = {
 	name: 'args-info',
 	description: 'Info about arguments',
 	guildOnly: true,
 	args: true,
 	execute(message, args) {
-		message.channel.send(`Command name: ${args}\nArguments: ${args.length}`);
+		const commandName = message.content.slice(prefix.length).trim().split(/ +/).shift().toLowerCase();
+		message.channel.send(`Command name: ${commandName}\nArguments: \`${args.join('` | `')}\`\nArgument amount: ${args.length}`);
 	},
 };
