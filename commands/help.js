c7aa35768f09fd4b2ad98861f35c2b40d764e231
NilsPvR <nilsundgrk@gmx.de>
2021-05-18 18:23:57 +0200

    help cmd with all cmds

--- commands/help.js
+++ commands/help.js
@@ -10,38 +10,46 @@ module.exports = {
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client; // all commands
-		const cateCommands = new Discord.Collection(); // categoryCommands
 
 		// returns an array of strings with the direct subfolders of a given absolute path
 		const getDirectories = source =>
 			readdirSync(source, { withFileTypes: true })
 				.filter(dirent => dirent.isDirectory()) // only keep directories
 				.map(dirent => dirent.name);
-
 		const categories = getDirectories(__dirname);
 
-		function getCategory(pathName) { // given path: string -> returns path lower then commands: array
-			const pathArr = pathName.split('\\');
-			while(pathArr.shift() !== 'commands') {
-				// nothing
-			}
-			return pathArr;
-		}
-
-		// --- the basic help command
+		// --- BASIC HELP COMMAND
 		if(!args.length) {
 			// could also make a js which contains category information -> get a name and stuff from that
 			data.push('Here\'s a list of available command categories:');
 			data.push(`${categories.join(', ')}`);
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a categorie`);
+			data.push(`\nYou can send \`${prefix}help [categorie name]\` to get info on a categorie`);
 			data.push(`Use \`${prefix}help all\` to get a complete list of all commands`);
 
 			return message.channel.send(data, { split: true });
 		}
-		// <TBD> -- complete command list
+		// -- COMPLETE COMMAND LIST
 		else if (args[0].toLowerCase() == 'all') {
 			data.push('Here\'s a list of all my commands:');
 			data.push('Now all categories with their according (subcategories and) commands should follow.... ');
+
+			for (const categorie of categories) { // each category
+				data.push(`\n__${categorie}:__ `);
+
+				const cateCommands = new Discord.Collection(); // categoryCommands
+				// get all the files for current category
+				const specificCmdFiles = readdirSync(pJoin('./commands', '/', categorie))
+					.filter(file => file.endsWith('.js'));
+
+				for (const file of specificCmdFiles) {
+					const command = require(`./${categorie}/${file}`);
+					// set a new item in the Collection
+					// with the key as the command name and the value as the exported module
+					cateCommands.set(command.name, command);
+				}
+
+				data.push(`\`${cateCommands.map(command => command.name).join('` | `')}\``);
+			}
 
 			return message.author.send(data, { split: true })
 				.then(() => {
@@ -53,11 +61,12 @@ module.exports = {
 					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
 				});
 		}
-		// --- info on a cateogry
+		// --- INFO ON A CATEGORY
 		// loop through the collection and compare each entry with the sent argument
-		const categorie = categories.find(category => args[0].toLowerCase() == category.toLowerCase());
+		const categorie = categories.find(category => args[0].toLowerCase() == category.toLowerCase()); // using function to make ever category lowercase
 
-		if (categorie) { // using function to make ever category lowercase
+		if (categorie) {
+			const cateCommands = new Discord.Collection(); // categoryCommands
 			// gets all commands for the category
 			const specificCmdFiles = readdirSync(pJoin('./commands', '/', categorie))
 				.filter(file => file.endsWith('.js'));
@@ -71,26 +80,11 @@ module.exports = {
 
 			data.push('Here\'s a list of all my commands:');
 			data.push(`\`${cateCommands.map(command => command.name).join('` | `')}\``);
-			/*
-			for (let i = 0; i < categories.length; i++) { // each category
-
-				const commandFiles = readdirSync(pJoin('./commands', '/', categories[i].toLowerCase())).filter(file => file.endsWith('.js'));
-
-				for (const file of commandFiles) {
-					const command = require(`./${categories[i]}/${file}`);
-					// set a new item in the Collection
-					// with the key as the command name and the value as the exported module
-					message.client.cateCommands.set(command.name, command);
-				}
-
-				data.push(`\`${commands.map(command => command.name).filter(command => message.client.cateCommands.includes(command))}\``);
-			}*/
-
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command`);
 
 			return message.channel.send(data, { split: true });
 		}
-		// --- info on a cmd
+		// --- INFO ON A CMD
 		const helpArg = args[0].toLowerCase(); // name of the command which should be descriped
 		const command = commands.get(helpArg) || commands.find(c => c.aliases && c.aliases.includes(helpArg));
 
