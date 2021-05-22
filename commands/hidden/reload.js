8f35168041bb2af9410013ea23fc5409e1870caf
NilsPvR <nilsundgrk@gmx.de>
2021-05-22 16:35:47 +0200

    major rewrite on reload command

--- commands/hidden/reload.js
+++ commands/hidden/reload.js
@@ -1,27 +1,72 @@
 const fs = require('fs');
+const { join } = require('path');
+
+const getAllFiles = (dirPath, arrayOfFiles) => {
+	// get all files in the current dir
+	const filesNfolders = fs.readdirSync(join(__dirname, dirPath));
+
+	arrayOfFiles = arrayOfFiles || []; // First time calling the method no array is given -> create a new one
+
+	filesNfolders.forEach(file => { // For all files or directories in the current dir
+		if (fs.statSync(join(__dirname, dirPath, '/', file)).isDirectory()) { // If directory
+			// recursive call
+			arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);
+		}
+		else if (file.endsWith('.js')) { // it's a js file
+			arrayOfFiles.push(join(__dirname, dirPath, '/', file)); // append absolute file path to array
+		}
+	});
+	return arrayOfFiles;
+};
 
 module.exports = {
 	name: 'reload',
 	aliases: ['r'],
 	description: 'Reload a command',
-	args: true,
 	execute(message, args) {
+		if (!args.length) { // reload all commands
+			let mSuccess = 0; // amount successful reloads
+			let mFail = 0; // amount unsuccesful reloads
+			const data = [];
+			const errorMsg = [];
+
+			for (const commandPath of getAllFiles('../../commands')) {
+				delete require.cache[require.resolve(commandPath)]; // delete out of the cache
+
+				try {
+					const newCommand = require(commandPath); // rerequire
+					message.client.commands.set(newCommand.name, newCommand); // set in collection
+					mSuccess++;
+				}
+				catch (error) {
+					console.error(error);
+					errorMsg.push(error.message);
+					mFail++;
+				}
+			}
+
+			if (mSuccess) data.push(`Reloaded ${mSuccess} modules succesfully.`);
+			if (mFail) data.push(`${mFail} modules could not be reloaded. The following errors have been cought:\n${errorMsg}`);
+
+			return message.channel.send(data);
+		}
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
+
 		if (!command) {
-			return message.channel.send(`No command found named \`${commandName}\`, ${message.author}!`);
+			return message.channel.send(`I couldn't find a command named \`${commandName}\``);
 		}
-		// subfolder not yest included !!!!
+		// subfolder not yet included !!!!
 		const commandFolders = fs.readdirSync('./commands');
 		const folderName = commandFolders.find(folder => fs.readdirSync(`./commands/${folder}`).includes(`${command.name}.js`));
 
-		delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
+		delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)]; // delete out of the cache
 
 		try {
-			const newCommand = require(`../${folderName}/${command.name}.js`);
-			message.client.commands.set(newCommand.name, newCommand);
+			const newCommand = require(`../${folderName}/${command.name}.js`); // rerequire
+			message.client.commands.set(newCommand.name, newCommand); // set in collection
 			message.channel.send(`Command \`${newCommand.name}\` was reloaded!`);
 		}
 		catch (error) {
