4ca504941a182e9ef508b7d25935cb41f598cbe9
NilsPvR <nilsundgrk@gmx.de>
2021-05-25 12:22:22 +0200

    actual ping command

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,8 +1,10 @@
 module.exports = {
 	name: 'ping',
-	description: 'pong',
-	cooldown: 5,
+	description: 'Vital information about me',
+	cooldown: 10,
 	execute(message) {
-		message.channel.send('Pong.');
+		message.channel.send(`Websocket heartbeat: ${message.client.ws.ping}ms.\nPinging...`).then(sent =>{
+			sent.edit(`Websocket heartbeat: ${message.client.ws.ping}ms.\nRoundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms.`);
+		});
 	},
 };
