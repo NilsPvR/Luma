9948a3c1b598ee205950dccd14dc19d5c837c02c
NilsPvR <nilsundgrk@gmx.de>
2021-05-27 00:10:17 +0200

    actually kicking people

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -7,8 +7,16 @@ module.exports = {
 		if (!message.mentions.users.size) {
 			return message.reply('you need to tag a user in order to kick them cuz I am stupido');
 		}
-		const taggedUser = message.mentions.users.first();
+		const taggedUser = message.mentions.members.first();
+		const username = taggedUser.user.username;
+		try {
+			message.channel.send(`As you wish. ${username} has been kicked!`);
+			taggedUser.kick();
+		}
+		catch (error) {
+			console.log(error);
+			message.channel.send(`I do not have permissions to kick ${username}`);
+		}
 
-		message.channel.send(`You wanted to kick: ${taggedUser.id}`);
 	},
 };
