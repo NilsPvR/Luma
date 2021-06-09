927a64ea196d5b860f48ef2e3c84a84bd1ee7ac9
NilsPvR <nilsundgrk@gmx.de>
2021-06-09 17:31:07 +0200

    improve mention and cmd av even more, comments

--- Lutil/mention.js
+++ Lutil/mention.js
@@ -1,15 +1,27 @@
 /*
 	These functions will return a member/user object based of the given argument
+
+	bothe methods should be used since a user could get found by the id even if a member could not get found (cuz they are not in the guild)
+		-> the getMember can find someone by their nickname, which the getUser can't
+			-> in most cases do smth like: user = getUser() ?? getMember().user
+
+	in rare instances the getUser() could only find someone case insensitive but the getMember() would be able to find a nickname case sensitive
+		-> leading into finding different people. For this case the user should be extracted from the member in the calling module
 */
 module.exports = {
 	async getUser(message, arg) {
 		if (!arg) return;
 		// get the id from mention or just plain id
 		const idMatches = arg.match(/^(?:<@!?)?(\d{18})(?:>)?$/);
-		if (!idMatches) {
-			// the user cache will be checked. If a user has been called by their id before then it will be in the cache
-			return message.client.users.cache.find(user => user.tag == arg || user.username == arg);
 
+		if (!idMatches) { // seach by name
+			// the user cache will be checked. If a user has been called by their id before then it will be in the cache
+			let foundUser = message.client.users.cache.find(user => user.tag == arg || user.username == arg);
+
+			// if nothing has been found try searching case insensitive
+			arg = arg.toLowerCase();
+			foundUser = foundUser ?? message.client.users.cache.find(user => user.tag.toLowerCase() == arg || user.username.toLowerCase() == arg);
+			return foundUser;
 		}
 
 		// fetch the user, Take the second element since the first is the whole thing
@@ -18,14 +30,22 @@ module.exports = {
 	},
 
 	async getMember(message, arg) {
+		if (!arg) return;
 		// get the id from mention or just plain id
 		const idMatches = arg.match(/^(?:<@!?)?(\d{18})(?:>)?$/);
 
-		if (!idMatches) {
+		if (!idMatches) { // search by name
 			// fetch all guild members instead of relying on cache
 			const members = await message.guild.members.fetch();
+
 			// if any of tag,name,nicknam macthes then return the member
-			return members.find(mem => mem.user.tag == arg || mem.user.username == arg || mem.nickname == arg);
+			let foundMember = members.find(mem => mem.user.tag == arg || mem.user.username == arg || mem.nickname == arg);
+
+			// if nothing has been found try seacrhing case insensitive
+			arg = arg.toLowerCase();
+			foundMember = foundMember ?? members.find(mem => mem.user.tag.toLowerCase() == arg
+				|| mem.user.username.toLowerCase() == arg || mem.nickname?.toLowerCase() == arg);
+			return foundMember;
 		}
 
 		// fetch the member, Take the second element since the first is the whole thing
--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,4 +1,5 @@
 const { getUser, getMember } = require('../../Lutil/mention');
+const { getID } = require('../../external-links.json').discord;
 module.exports = {
 	name: 'avatar',
 	aliases: ['av', 'profilepicture', 'pfp'],
@@ -27,7 +28,8 @@ module.exports = {
 			};
 		}
 
-
+		/* bothe methods are used since a user could get found by the id even if a member could not get found (cuz they are not in the guild)
+			the getMember can find someone by their nickname, which the getUser can't */
 		// promise returns the userobject
 		let user = await getUser(message, args[0]);
 		// promise returns the memberobject
@@ -37,7 +39,14 @@ module.exports = {
 		if (!user && !member) { // nothing found
 			return {
 				flag: 'error',
-				description: `Unable to find a someone with the id or name: ${args[0]}`,
+				description: `Unable to find someone with the ID or name: ${args[0]}`,
+
+				fields: [
+					{
+						name: '\u200b',
+						value: `Try using their [ID](${getID})`,
+					},
+				],
 			};
 		}
 		else if (user && !member) { // user found but no member -> simple information
@@ -55,7 +64,11 @@ module.exports = {
 			};
 		}
 		// no user but member or both are found
-		user = user ?? member.user; // If the user hasn't been found yet get it from the member
+
+		/* even if the user has been found get it from the member
+			-> in rare instances the getUser() could only find someone case insensitive but the getMember() would be able to find a nickname case sensitive
+				-> leading into finding different people */
+		user = member.user;
 		const footerText = member.nickname ? (user.tag + ' - ') : ''; // if nickname -> show tag, else none
 
 		return {
