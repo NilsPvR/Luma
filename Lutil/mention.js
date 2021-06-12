9f758df3db4b5a9c917c52677b22b018bfe611a5
NilsPvR <nilsundgrk@gmx.de>
2021-06-12 23:18:13 +0200

    be able to skip when using a tag

--- Lutil/mention.js
+++ Lutil/mention.js
@@ -9,19 +9,30 @@
 		-> leading into finding different people. For this case the user should be extracted from the member in the calling module
 */
 const { confirm } = require('../Lutil/confirmperson');
+// only allow id's or mentions with y/yes at the end| (id)(y)
+const idregex = new RegExp(/^(?:<@!?)?(\d{18})(?:>)? ?(y|yes)?$/i);
+// only allow discord tags | (tag)(y)
+const tagregex = new RegExp(/(^.+#\d{4}) ?(y|yes)$/i);
 
 async function fgetUser(message, arg) {
 	// get the id from mention or just plain id, check for 'y' at the end
-	const idMatches = arg.match(/^(?:<@!?)?(\d{18})(?:>)? ?(y|Y)?$/);
+	const idMatches = arg.match(idregex);
+	const tagMatches = arg.match(tagregex);
 
 	if (!idMatches) { // seach by name
 		// the user cache will be checked. If a user has been called by their id before then it will be in the cache
-		let foundUser = message.client.users.cache.find(user => user.tag == arg || user.username == arg);
+		let foundUser = message.client.users.cache.find(user => user.tag == arg || user.username == arg
+			|| user.tag == tagMatches?.[1]); // if they use Metzok#6146yes -> get the tag like this
 
 		// if nothing has been found try searching case insensitive
 		arg = arg.toLowerCase();
-		foundUser = foundUser ?? message.client.users.cache.find(user => user.tag.toLowerCase() == arg || user.username.toLowerCase() == arg);
-		return { user: foundUser };
+		foundUser = foundUser ?? message.client.users.cache.find(user => user.tag.toLowerCase() == arg || user.username.toLowerCase() == arg
+			|| user.tag.toLowerCase() == tagMatches?.[1]?.toLowerCase()); // if they use Metzok#6146yes -> get the tag like this
+
+		return {
+			user: foundUser,
+			skipConfirm:  tagMatches?.[2], // is y/yes or undefined
+		};
 	}
 	else {
 		// fetch the user, Take the second element since the first is the whole thing
@@ -29,9 +40,10 @@ async function fgetUser(message, arg) {
 			.catch((error) => {
 				console.error(`Error initiated in mention.js: ${error.message}`);
 			});
+
 		return {
 			user: returnUser,
-			skipConfirm: idMatches[2]?.toLowerCase(), // is y or undefined
+			skipConfirm: idMatches[2], // is y/yes or undefined
 		};
 
 	}
@@ -41,20 +53,27 @@ async function fgetUser(message, arg) {
 async function fgetMember(message, arg) {
 	if (message.channel.type === 'dm') return;
 	// get the id from mention or just plain id, check for 'y' at the end
-	const idMatches = arg.match(/^(?:<@!?)?(\d{18})(?:>)? ?(y|Y)?$/);
+	const idMatches = arg.match(idregex);
+	const tagMatches = arg.match(tagregex);
 
 	if (!idMatches) { // search by name
 		// fetch all guild members instead of relying on cache
 		const members = await message.guild.members.fetch();
 
 		// if any of tag,name,nicknam macthes then return the member
-		let foundMember = members.find(mem => mem.user.tag == arg || mem.user.username == arg || mem.nickname == arg);
+		let foundMember = members.find(mem => mem.user.tag == arg || mem.user.username == arg || mem.nickname == arg
+			|| mem.user.tag == tagMatches?.[1]); // if they use Metzok#6146yes -> get the tag like this
 
 		// if nothing has been found try seacrhing case insensitive
 		arg = arg.toLowerCase();
 		foundMember = foundMember ?? members.find(mem => mem.user.tag.toLowerCase() == arg
-			|| mem.user.username.toLowerCase() == arg || mem.nickname?.toLowerCase() == arg);
-		return { member: foundMember };
+			|| mem.user.username.toLowerCase() == arg || mem.nickname?.toLowerCase() == arg
+			|| mem.user.tag.toLowerCase() == tagMatches?.[1]?.toLowerCase()); // if they use Metzok#6146yes -> get the tag like this
+
+		return {
+			member: foundMember,
+			skipConfirm: tagMatches?.[2],
+		};
 	}
 	else {
 		// fetch the member, Take the second element since the first is the whole thing
@@ -62,9 +81,10 @@ async function fgetMember(message, arg) {
 			.catch((error) => {
 				console.error(`Error initiated in mention.js: ${error.message}`);
 			});
+
 		return {
 			member: returnMember,
-			skipConfirm: idMatches[2]?.toLowerCase(), // is y or undefined
+			skipConfirm: idMatches[2], // is y/yes or undefined
 		};
 
 	}
@@ -74,8 +94,11 @@ module.exports = {
 	async getUser(message, arg, shouldFullConfirm) {
 		if (!arg) return;
 		const returnObj = await fgetUser(message, arg);
+		const tagMatches = arg.match(tagregex);
 
-		if (returnObj.skipConfirm == 'y') shouldFullConfirm = false;
+		// just test if not empty, no need to compare with 'y'
+		if (returnObj.skipConfirm) shouldFullConfirm = false;
+		arg = tagMatches?.[1] ?? arg; // if they used Metzok#6146yes -> then only tag
 
 		// aks user for confirmation if shouldConfirm, otherwise only check if member/user exists
 		if (await confirm(message, arg, returnObj.user, shouldFullConfirm)) return returnObj.user; // confirmed
@@ -85,8 +108,11 @@ module.exports = {
 	async getMember(message, arg, shouldFullConfirm) {
 		if (!arg) return;
 		const returnObj = await fgetMember(message, arg);
+		const tagMatches = arg.match(tagregex);
 
-		if (returnObj.skipConfirm == 'y') shouldFullConfirm = false;
+		// just test if not empty, no need to compare with 'y'
+		if (returnObj.skipConfirm) shouldFullConfirm = false;
+		arg = tagMatches?.[1] ?? arg; // if they used Metzok#6146yes -> then only tag
 
 		// aks user for confirmation if shouldConfirm, otherwise only check if member/user exists
 		if (await confirm(message, arg, null, returnObj.member, shouldFullConfirm)) return returnObj.member; // confirmed
@@ -98,18 +124,22 @@ module.exports = {
 
 		const both = {};
 		const returnObjMem = await fgetMember(message, arg);
+		let returnObjUs;
+		const tagMatches = arg.match(tagregex);
+
 		both.member = returnObjMem.member;
 
 		if (!both.member) { // no member found check for user
-			const returnObjUs = await fgetUser(message, arg);
+			returnObjUs = await fgetUser(message, arg);
 			both.user = returnObjUs.user;
 		}
 		else { // member found -> just get user from member
 			both.user = both.member.user;
 		}
 
-		const idMatches = arg.match(/^(?:<@!?)?(\d{18})(?:>)? ?(y|Y)?$/);
-		if (idMatches && idMatches[2] == 'y') shouldFullConfirm = false;
+
+		if (returnObjMem.skipConfirm || returnObjUs?.skipConfirm) shouldFullConfirm = false;
+		arg = tagMatches?.[1] ?? arg; // if they used Metzok#6146yes -> then only tag
 
 		// aks user for confirmation if shouldConfirm, otherwise only check if member/user exists
 		if (await confirm(message, arg, both.user, both.member, shouldFullConfirm)) return both; // confirmed
