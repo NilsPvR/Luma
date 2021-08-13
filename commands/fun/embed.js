cc7343ccddd2a4b7f9f48c5da591f4ab23f01334
NilsPvR <nilsundgrk@gmx.de>
2021-08-13 12:17:54 +0200

    first two slash commands

--- commands/fun/embed.js
+++ commands/fun/embed.js
@@ -1,11 +1,22 @@
 module.exports = {
 	name: 'embed',
 	template: 'requester',
+
 	async execute() {
 		return {
 			flag: 'error',
 			title: 'Nice title',
 			description: 'Nice embeded message',
 		};
+	},
+
+
+	slashCmdData: {
+		name: 'embed',
+		description: 'Sends an embeded message.',
+	},
+
+	async slashExecute() {
+		return this.execute();
 	},
 };
--- commands/info/server.js
+++ commands/info/server.js
@@ -1,21 +1,30 @@
 module.exports = {
-	name: 'server',
-	aliases: ['serverinfo'],
+	name: 'serverinfo',
+	aliases: ['server', 'guildinfo', 'guild'],
 	description: 'Detailed information about the server',
 	guildOnly: true,
 	template: 'simple',
-	async execute(message) {
+	async execute(msg_intact) {
 		return {
-			title: message.guild.name,
+			title: msg_intact.guild.name,
 
 			fields: {
 				name: '\u200b',
-				value: 'Members: ' + message.guild.memberCount,
+				value: 'Members: ' + msg_intact.guild.memberCount,
 			},
 
 			footer: {
-				text: 'Serverinfo for: ' + message.guild.id,
+				text: 'Serverinfo for: ' + msg_intact.guild.id,
 			},
 		};
+	},
+
+	slashCmdData: {
+		name: 'serverinfo',
+		description: 'Detailed information about the server.',
+	},
+
+	async slashExecute(client, interaction) {
+		return this.execute(interaction);
 	},
 };
